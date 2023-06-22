2ac108072c31e6c07a2d63ac10ebb0795e357e9f
meriem23 <esra.msallem@gmail.com>
2023-06-22 16:16:44 +0100

    2nd commit

--- src/components/counter-two/counterTwo.test.tsx
+++ src/components/counter-two/counterTwo.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import CounterTwo from "./counterTwo";
 
 describe("Counter two", () => {
@@ -8,7 +9,8 @@ describe("Counter two", () => {
     expect(textElement).toBeInTheDocument();
   });
   //we don't care about the functions increment and decrement, all we need to test is that they are fired when the buttons are clicked, so we are going to use mock functions from jest to test
-  test("handlers are called", () => {
+  test("handlers are called", async () => {
+    user.setup();
     const incrementHandler = jest.fn();
     const decrementHandler = jest.fn();
     render(
@@ -20,5 +22,10 @@ describe("Counter two", () => {
     );
     const incrementButton = screen.getByRole("button", { name: "Increment" });
     const decrementButton = screen.getByRole("button", { name: "Decrement" });
+    await user.click(incrementButton);
+    await user.click(decrementButton);
+
+    expect(incrementHandler).toHaveBeenCalledTimes(1);
+    expect(decrementHandler).toHaveBeenCalledTimes(1);
   });
 });
